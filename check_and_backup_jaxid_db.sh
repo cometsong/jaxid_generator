af2174f70d367582e68f3f2cc949f2e60d017d23
cometsong <benjamin@cometsong.net>
2015-12-21 15:09:06 -0500

    db backup script to use in crontab for regular (daily) backups

---
+++ check_and_backup_jaxid_db.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------------------------------
+#       Check if jaxID db has been modified, then back it up to repo           -
+#-------------------------------------------------------------------------------
+
+## check if db has been changed
+DBPATH=/var/www/django/jaxid_generator
+DBNAME=db.sqlite3
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Run it! ~~~~~
+cd $DBPATH
+MOD=$(git status --porcelain --untracked=no | grep $DBNAME | cut -c2,2)
+
+if [[ "$MOD" == "M" ]]; then
+    ## git add, git commit; git push origin master
+    git add -v $DBNAME
+    git commit -m "latest db update backup"
+    git push --porcelain origin master
+fi
+
