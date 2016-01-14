6b1af8c8c4a9556c202bc573733f3e7b19f5f93a
cometsong <benjamin@cometsong.net>
2016-01-14 11:57:29 -0500

    mod check_and_backup_jaxid_db.sh to add 'migrations'

--- check_and_backup_jaxid_db.sh
+++ check_and_backup_jaxid_db.sh
@@ -16,6 +16,7 @@ MOD=$(git status --porcelain --untracked=no | grep $DBNAME | cut -c2,2)
 if [[ "$MOD" == "M" ]]; then
     ## git add, git commit; git push origin master
     git add -v $DBNAME
+    git add -v id_generate/migrations/
     git commit -m "latest db update backup"
     git push --porcelain origin master
 fi
