4f899a17f1a12fe5ab4cd3ca7c95a7cc507ed628
cometsong <benjamin@cometsong.net>
2017-06-09 16:40:02 -0400

    models: mod parent_jaxid required

--- id_generate/models.py
+++ id_generate/models.py
@@ -89,9 +89,10 @@ class JAXIdDetail(models.Model):
             # help_text="A unique ID string for every sample.",
             )
     parent_jaxid = models.CharField('Parent JAXid',
-            max_length=6, blank=True, default='', unique=False,
-            help_text="Parent ID string or leave blank if has no parent; "
-                      "RECVD for newly received samples.",
+            max_length=9, default='', unique=False,
+            help_text="Parent ID string of source JAXid; or use "
+                      "'received' for newly received samples,"
+                      "'pool' for pools of libraries.",
             )
     project_code = models.ForeignKey(ProjectCode, to_field='code',)
     collab_id = models.TextField('Collaborator ID',
