a4d6971b8f7c41e6adc52bdad7141a1decceb853
cometsong <benjamin@cometsong.net>
2017-06-09 16:38:58 -0400

    mod generator suit menu url spelling; add to urls.py

--- generator/apps.py
+++ generator/apps.py
@@ -11,7 +11,6 @@ class SuitConfig(DjangoSuitConfig):
     site_header = site_title
     index_title = verbose_name
 
-
     layout = 'vertical'
     list_per_page = 35
 
@@ -21,8 +20,12 @@ class SuitConfig(DjangoSuitConfig):
     menu = (
             ParentItem(
                 label='JAXid Record List',
-                url='/{}/admin/id_generate/jaxiddetail'.format(APP_NAME),
+                url='/{}/manage/id_generate/jaxiddetail'.format(APP_NAME),
                 icon='fa th-list'),
+            # ParentItem(
+            #     label='JAXid Record List model',
+            #     model='id_generate.jaxiddetail',
+            #     icon='fa th-list'),
             ParentItem('Reference Data',
                 use_first_child_url=False,
                 url='',
@@ -35,8 +38,12 @@ class SuitConfig(DjangoSuitConfig):
                 icon='fa th-list'),
             ParentItem(
                 label='Generate new JAXid''s',
-                url='/{}/admin/id_generate/jaxiddetail/import/'.format(APP_NAME),
+                url='/{}/manage/id_generate/jaxiddetail/import/'.format(APP_NAME),
                 icon='fa open-file'),
+            # ParentItem(
+            #     label='Import JAXid''s',
+            #     url='/{}/manage/import_jaxids'.format(APP_NAME),
+            #     icon='fa open-file'),
             ParentItem(
                 label='Authorization',
                 children=[
@@ -68,22 +75,22 @@ class SuitConfig(DjangoSuitConfig):
         },
     }
 
-#     # form_size setting can be overridden in ModelAdmin using suit_form_size parameter
-#     #
-#     # Example:
-#     # ----------------------------------------------
-#     # suit_form_size = {
-#     #     'default': 'col-xs-12 col-sm-2', 'col-xs-12 col-sm-10',
-#     #     'fields': {
-#     #          'field_name': SUIT_FORM_SIZE_LARGE,
-#     #          'field_name2': SUIT_FORM_SIZE_X_LARGE,
-#     #      },
-#     #      'widgets': {
-#     #          'widget_class_name': SUIT_FORM_SIZE_FULL,
-#     #          'AdminTextareaWidget': SUIT_FORM_SIZE_FULL,
-#     #      },
-#     #      'fieldsets': {
-#     #          'fieldset_name': SUIT_FORM_SIZE_FULL,
-#     #          'fieldset_name2': SUIT_FORM_SIZE_FULL,
-#     #      }
-#     # }
+    # form_size setting can be overridden in ModelAdmin using suit_form_size parameter
+    #
+    # Example:
+    # ----------------------------------------------
+    # suit_form_size = {
+    #     'default': 'col-xs-12 col-sm-2', 'col-xs-12 col-sm-10',
+    #     'fields': {
+    #          'field_name': SUIT_FORM_SIZE_LARGE,
+    #          'field_name2': SUIT_FORM_SIZE_X_LARGE,
+    #      },
+    #      'widgets': {
+    #          'widget_class_name': SUIT_FORM_SIZE_FULL,
+    #          'AdminTextareaWidget': SUIT_FORM_SIZE_FULL,
+    #      },
+    #      'fieldsets': {
+    #          'fieldset_name': SUIT_FORM_SIZE_FULL,
+    #          'fieldset_name2': SUIT_FORM_SIZE_FULL,
+    #      }
+    # }
--- generator/urls.py
+++ generator/urls.py
@@ -3,9 +3,9 @@ from django.contrib import admin
 from id_generate import generate
 
 urlpatterns = [
-    url(r'^admin/generate_new_jaxids$',
+    url(r'^manage/import_jaxids$',
         generate.generate_new_ids),
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^manage/', include(admin.site.urls)),
     # url(r'^files/', include('filer.urls')),
-    # url(r'^', include('id_generate.urls')),
+    url(r'^', include('id_generate.urls')),
 ]
