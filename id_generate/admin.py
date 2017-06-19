06c21f52c73aecda426fca33faf4985fa1b80e04
cometsong <benjamin@cometsong.net>
2017-06-19 09:56:12 -0400

    admin: add Session View

--- id_generate/admin.py
+++ id_generate/admin.py
@@ -1,5 +1,7 @@
+import pprint
 from django.contrib import admin
 from django.contrib.admin.sites import AdminSite
+from django.contrib.sessions.models import Session
 
 from import_export import fields, widgets
 from import_export.admin import ExportActionModelAdmin, ImportExportModelAdmin, \
@@ -35,6 +37,16 @@ ID_DETAIL_FIELDS = JAXIdDetail.all_field_names()
 admin.site.site_header = 'Mbiome Core JAXid Tracking Administration'
 admin.site.site_title = 'Mbiome Core JAXid Tracking'
 admin.site.index_title = 'JAXid Generator'
+
+
+class SessionAdmin(admin.ModelAdmin):
+    def _session_data(self, obj):
+        return pprint.pformat(obj.get_decoded()).replace('\n', '<br>\n')
+    _session_data.allow_tags=True
+    list_display = ['session_key', '_session_data', 'expire_date']
+    readonly_fields = ['_session_data']
+    exclude = ['session_data']
+admin.site.register(Session, SessionAdmin)
 
 @admin.register(ProjectCode)
 class ProjectCodeAdmin(admin.ModelAdmin):
