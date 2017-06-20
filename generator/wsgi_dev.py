cb14c9d5fa0d7b31ae32ca49d6360a03c69d67c3
cometsong <benjamin@cometsong.net>
2017-06-20 13:49:18 -0400

    update wsgi_dev paths

--- generator/wsgi_dev.py
+++ generator/wsgi_dev.py
@@ -2,9 +2,9 @@
 WSGI config for jaxid_generator project.
 """
 # django virtualenv variables
-path_app = "/var/www/apps/jaxid_generator"
+path_app = "/var/www/apps/jaxid_gen_dev"
 app_settings = "generator.settings.dev"
-path_site_packages = path_app + "/lib/python3.6/site-packages"
+path_site_packages = "/var/www/apps/jaxid_generator" + "/lib/python3.6/site-packages"
 
 # semi-global wsgi script
 import os
