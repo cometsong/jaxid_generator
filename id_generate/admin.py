1e8b71985875368e314c44aec995caac1fdf8bf7
cometsong <benjamin@cometsong.net>
2018-01-31 15:43:28 -0500

    admin import/generate refactor: mb IdChangeList and BaseImportAdmin to separate files

--- id_generate/admin.py
+++ id_generate/admin.py
@@ -40,6 +40,8 @@ from .forms import (
         ProjectCodeForm
         )
 from .import_data import DetailResource, BoxIdResource
+from .admin_import_mixin import BaseImportAdmin
+from .changelist import IdChangeList
 
 # implement and register databrowse for external read-only access
 import django_databrowse
@@ -247,112 +249,7 @@ class JAXIdDetailAdmin(ImportExportModelAdmin, RelatedFieldAdmin):
 idadmin.register(JAXIdDetail, JAXIdDetailAdmin)
 
 
-
-class IdChangeList(ChangeList):
-    """ Override default Changelist to check for request attr 'imported_ids' """
-
-    def get_queryset(self, request):
-        """ Returns queryset. Default implementation respects applied search and filters.
-        This override returns request attr 'imported_queryset' if exists
-        """
-        try:
-            qs = super().get_queryset(request)
-            # print(f'DEBUG: {settings.APP_NAME}: qs - super_qset length: {qs.count()}')
-            # print(f'DEBUG: {funcname()} - getting qset_attr')
-            pks_attr = request.POST.getlist('imported_ids')
-            if pks_attr:
-                print(f'DEBUG: {funcname()} - pks_attr: {pks_attr!s}')
-                print(f'DEBUG: {funcname()} - filtering qset super')
-                qs = qs.filter(pk__in=pks_attr).reverse()
-        except Exception as e:
-            print(f'ERROR: {funcname()} - Exception: {e.response}')
-        finally:
-            # print(f'DEBUG: {funcname()} - qset length: {qs.count()}')
-            return qs
-
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # qs = self.get_queryset(request) #AttributeError
-        # self.queryset = self.get_queryset(request) #NameError requeset not defined
-
-
-class BaseImportAdmin(ImportExportModelAdmin):
-    """Abstract Base Admin mixin for generic import/export methods """
-
-    def export_imported_file(self, request, *args, **kwargs):
-        try:
-            print(f'DEBUG: {funcname()} beginning')
-            input_format = request.POST.get('input_format')
-            orig_filename = request.POST.get('original_file_name')
-            new_name_prefix = 'generated'
-            export_filename = '_'.join([new_name_prefix, orig_filename])
-            export_filename, subnum = re.subn(' ', '_', export_filename)
-            print(f'DEBUG: {funcname()} - format: {input_format}, export_name: {export_filename}')
-
-            formats = self.get_export_formats()
-            file_format = formats[int(input_format)]()
-            # print(f'DEBUG: {funcname()} file_format: {file_format!s}')
-
-            queryset = self.get_export_queryset(request)
-            # print(f'DEBUG: {funcname()} queryset: {queryset!s}')
-
-            export_data = self.get_export_data(file_format, queryset, request=request)
-            # print(f'DEBUG: {funcname()} dataset length: {len(export_data)!s}')
-            file_baseurl = settings.IMPORTED_FILE_PATH
-            file_basepath = os.path.join(settings.HTML_DIR, settings.IMPORTED_FILE_PATH)
-            filepath = os.path.join(file_basepath, export_filename)
-            fileurl = os.path.join('/', file_baseurl, export_filename)
-            # print(f'DEBUG: {funcname()} filepath: {filepath!s}')
-            # print(f'DEBUG: {funcname()} fileurl: {fileurl!s}')
-            with open(filepath, 'wb') as exp:
-                exp.write(export_data)
-        except Exception as e:
-            print(f'ERROR: {funcname()}: {e.message!s}')
-            # raise e
-        finally:
-            return fileurl
-
-
-    def add_export_message(self, request, file_url=None):
-        opts = self.model._meta
-        if file_url:
-            filename = os.path.basename(file_url)
-            export_message = f'The ids imported into {opts.verbose_name_plural}, can <em>now</em> be ' \
-                             f'downloaded with this link: <a href={file_url!s}>"{filename}"</a>'
-            messages.info(request, export_message)
-
-
-    def process_result(self, result, request):
-        """override import-export admin method to redirect to immediate export url post-import"""
-        print(f'DEBUG: entering overridden process_result')
-        try:
-            print(f'DEBUG: {funcname()} calling super process_result')
-            sup = super().process_result(result, request)
-            imported_ids = [row.object_id for row in result.rows]
-            print(f'DEBUG: {funcname()} imported_ids: {imported_ids!s}')
-
-            if request.method == 'POST' and request.POST:
-                request.POST = request.POST.copy() # mutable via copy
-                request.POST.setlist('imported_ids', imported_ids)
-                # print(f'DEBUG: req POST: {request.POST!s}')
-
-            export_file_url = self.export_imported_file(request)
-            from django.contrib import messages
-            self.add_export_message(request, file_url=export_file_url)
-        except Exception as e:
-            print(f'ERROR: {funcname()} request copy/mod/reinstate yuckiness: {e.message!s}')
-            # raise e
-        finally:
-            return self.changelist_view(request, extra_context=None)
-
-
-    def get_changelist(self, request, **kwargs):
-        """ Returns the ChangeList class for use on the changelist page. """
-        return IdChangeList  # override with local class
-
-
-
+# class BoxIdAdmin(BaseImportAdmin):
 class BoxIdAdmin(BaseImportAdmin, RelatedFieldAdmin):
     resource_class = BoxIdResource
 
@@ -390,9 +287,9 @@ class BoxIdAdmin(BaseImportAdmin, RelatedFieldAdmin):
     ordering = ['-creation_date']
     formats = (base_formats.XLSX,)
 
+    def get_changelist(self, request, **kwargs):
+        """ Returns the ChangeList class for use on the changelist page. """
+        return IdChangeList  # override with local class
+
 
 idadmin.register(BoxId, BoxIdAdmin)
-
-
-
-
