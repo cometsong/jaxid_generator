218d3f14529132086abca006cc18eb0f22782414
cometsong <benjamin@cometsong.net>
2018-02-01 14:09:17 -0500

    updates to crontab dumpdata_django_db.sh

--- dumpdata_django_db.sh
+++ dumpdata_django_db.sh
@@ -17,28 +17,30 @@ function get_datestamp_ymd () { #{{{ ### from cometsong_util_functions.sh ###
     echo $(get_datestamp $FORMAT)
 } #}}}
 
-### from utils.sh ###
+### print/err/die funcs from cometsong_util_functions.sh ### #{{{
 # print: echoes all args to stdout
-function print() { printf '%b\n' "$@"; }
+    function print() { printf '%b\n' "$@"; }
 # err: echoes all args to stderr
-function err()   { print "$*" >&2; }
+    function err()   { print "$*" >&2; }
 # debug: calls `err`
-function debug() { err "$@"; }
+    function debug() { err "$@"; }
 # die: sends all args to `err` then exits >0
-function die()   { err "$@" && exit 11; }
+    function die()   { err "$@" && exit 11; }
+# }}}
 
-function exclusion_args() { #{{{
-    # prepend "-e " to each element in passed string
+function prepend_items() { #{{{
+    # Usage: prepend_items 'delimiter' "list of args to prepend w/ delimiter"
+    #        Include single-quoted <space> in delimiter if desired.
+    Delim=${1} && shift || echo "ERROR in 'joined' delimiter missing." >2
     List=( $@ )
-    Prepended=""
-    for element in ${List[@]} ; do
-        Prepended+="-e $element "
-    done
-    echo "$Prepended"
+    [[ "$Delim" =~ '-' ]] && Delim=${Delim//-/\\-} && hyphens=True #escape hyphens for 'printf'
+    prepended=$(printf "${Delim}%s " ${List[@]});
+    [[ -n ${hyphens} ]] && prepended=${prepended//\\-/-}
+    echo "$prepended"
 } #}}}
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Variables ~~~~~
-App=${1:-"jaxid_dev"}
+App=${1:="jaxid_dev"}
 
 Today=$(get_datestamp_ymd)
 
@@ -46,16 +48,18 @@ Format="json"
 DjangoAppPath="/var/www/apps/${App}"
 BackupFile="${DjangoAppPath}/backups/${Today}.db.${App}.${Format}"
 
-#Exclusions=$(exclusion_args "sessions contenttypes auth admin.logentry")
-Exclusions=$(exclusion_args "sessions contenttypes auth")
+#Exclusions=$(prepend_items '-e ' sessions contenttypes auth admin.logentry)
+Exclusions=$(prepend_items '-e ' sessions contenttypes auth)
+
+EmailTo=${USER}@jax.org
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Back Me Up! Dump Me Out! ~~~~~
 
-print "Beginning data dump of default django database."
+print "Beginning data dump of '$App' django database."
 
 [[ -d ${DjangoAppPath} ]] && cd ${DjangoAppPath} && . ./bin/activate
 if [[ $? == 0 ]]; then
-    #--natural-foreign \
+#--natural-foreign \
 manage.py dumpdata \
     --format $Format \
     --natural-primary \
@@ -63,9 +67,31 @@ manage.py dumpdata \
     ${Exclusions} \
     -o ${BackupFile} \
     \
-    && gzip -v ${BackupFile}
+    && gzip -vf9N ${BackupFile}
 fi
 
-print "Database dump now at: ${BackupFile}.gz"
+if [[ -f "${BackupFile}.gz" ]]; then
+    print "Database dump now at: ${BackupFile}.gz"
+    Msg="Database dump attached: ${BackupFile}.gz"
+    Subj="'$App' database backup completed on ${Today}"
+    File="-a ${BackupFile}.gz"
+else
+    print "Database dump ERROR??: ${BackupFile}.gz"
+    Msg="Database dump error?
+    ${BackupFile}.gz
+    Please find out why that file does not exist."
+    Subj="'$App' database backup ERROR on ${Today}"
+    File=""
+fi
+
+print Emailing the backup results or error msg....
+
+echo "${Msg}" | \
+mail ${File}    \
+-s "${Subj}"    \
+${EmailTo}
+
+print DB Backup Script completed
+
 
 # vim: ft=sh fdm=marker:
