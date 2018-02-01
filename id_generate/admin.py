5a47bb7495de814efe26900dd76a4dc26e89f3f1
cometsong <benjamin@cometsong.net>
2018-02-01 09:07:46 -0500

    Refactor JAXIdDetailAdmin to use BaseImportAdmin for DRY happiness

--- id_generate/admin.py
+++ id_generate/admin.py
@@ -127,7 +127,7 @@ class NucleicAcidTypeAdmin(admin.ModelAdmin):
     ordering = ['code']
 idadmin.register(NucleicAcidType, NucleicAcidTypeAdmin)
 
-class JAXIdDetailAdmin(ImportExportModelAdmin, RelatedFieldAdmin):
+class JAXIdDetailAdmin(BaseImportAdmin, RelatedFieldAdmin):
     resource_class = DetailResource
 
     def has_delete_permission(self, request, obj=None):
@@ -174,77 +174,9 @@ class JAXIdDetailAdmin(ImportExportModelAdmin, RelatedFieldAdmin):
     JAXIdDetail.sequencing_type_code.admin_order_field = 'sequencing_type'
     JAXIdDetail.sample_type_code.admin_order_field = 'sample_type'
 
-
-    def export_imported_file(self, request, *args, **kwargs):
-        try:
-            print(f'DEBUG: {funcname()} beginning')
-            input_format = request.POST.get('input_format')
-            orig_filename = request.POST.get('original_file_name')
-            new_name_prefix = 'generated'
-            export_filename = '_'.join([new_name_prefix, orig_filename])
-            export_filename, subnum = re.subn(' ', '_', export_filename)
-            print(f'DEBUG: {funcname()} - format: {input_format}, export_name: {export_filename}')
-
-            formats = self.get_export_formats()
-            file_format = formats[int(input_format)]()
-            # print(f'DEBUG: {funcname()} file_format: {file_format!s}')
-
-            queryset = self.get_export_queryset(request)
-            # print(f'DEBUG: {funcname()} queryset: {queryset!s}')
-
-            export_data = self.get_export_data(file_format, queryset, request=request)
-            # print(f'DEBUG: {funcname()} dataset length: {len(export_data)!s}')
-            file_baseurl = settings.IMPORTED_FILE_PATH
-            file_basepath = os.path.join(settings.HTML_DIR, settings.IMPORTED_FILE_PATH)
-            filepath = os.path.join(file_basepath, export_filename)
-            fileurl = os.path.join('/', file_baseurl, export_filename)
-            # print(f'DEBUG: {funcname()} filepath: {filepath!s}')
-            # print(f'DEBUG: {funcname()} fileurl: {fileurl!s}')
-            with open(filepath, 'wb') as exp:
-                exp.write(export_data)
-        except Exception as e:
-            print(f'ERROR: {funcname()}: {e.message!s}')
-            # raise e
-        finally:
-            return fileurl
-
-
-    def add_export_message(self, request, file_url=None):
-        opts = self.model._meta
-        if file_url:
-            filename = os.path.basename(file_url)
-            export_message = f'The ids imported into {opts.verbose_name_plural}, can <em>now</em> be ' \
-                             f'downloaded with this link: <a href={file_url!s}>"{filename}"</a>'
-            messages.info(request, export_message)
-
-
     def get_changelist(self, request, **kwargs):
         """ Returns the ChangeList class for use on the changelist page. """
         return IdChangeList  # override with local class
-
-
-    # override import-export admin method to redirect to immediate export url post-import
-    def process_result(self, result, request):
-        print(f'DEBUG: entering overridden process_result')
-        try:
-            print(f'DEBUG: {funcname()} calling super process_result')
-            sup = super().process_result(result, request)
-            imported_ids = [row.object_id for row in result.rows]
-            print(f'DEBUG: {funcname()} imported_ids: {imported_ids!s}')
-
-            if request.method == 'POST' and request.POST:
-                request.POST = request.POST.copy() # mutable via copy
-                request.POST.setlist('imported_ids', imported_ids)
-                # print(f'DEBUG: req POST: {request.POST!s}')
-
-            export_file_url = self.export_imported_file(request)
-            from django.contrib import messages
-            self.add_export_message(request, file_url=export_file_url)
-        except Exception as e:
-            print(f'ERROR: {funcname()} request copy/mod/reinstate yuckiness: {e.message!s}')
-            # raise e
-        finally:
-            return self.changelist_view(request, extra_context=None)
 
 idadmin.register(JAXIdDetail, JAXIdDetailAdmin)
 
